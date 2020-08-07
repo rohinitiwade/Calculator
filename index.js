5b8fb6f9c31990473ee0d15f2e66f9c757f683e5
rohinitiwade <68542384+rohinitiwade@users.noreply.github.com>
2020-08-07 17:20:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImage1 = "dice" + randomNumber1 + ".png";
-var imagepath1 = "images/" + randomImage1;
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src" ,imagepath1);
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImage2 = "dice" + randomNumber2 + ".png";
-var imagepath2 = "images/" + randomImage2;
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src" ,imagepath2);
-
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = ("🚩 Player 1 Wins!");
+function submitVal(){
+var yname = $("#yname").val();
+var pname = $("#pname").val();
+if(yname == '')
+ $("#yname1").show();
+ if(pname == '')
+  $("#uname1").show();
+var random = Math.floor(Math.random() * 100);
+if(yname != '' && pname != '')
+{$("#yname1").hide();
+ $("#uname1").hide();
+document.querySelector('#showResult').innerHTML=yname + ' Loves 💞 ' + random + ' % To ' + pname;
 }
-
-else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = (" Player 2 Wins! 🚩");
-}
-else if(randomNumber1 == randomNumber2){
-    document.querySelector("h1").innerHTML = ("Draw! ");
 }
